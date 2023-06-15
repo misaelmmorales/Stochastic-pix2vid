e8387e437a97532f14be100cd18d660ab7b16e03
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-06-15 17:18:21 -0600

    update

--- simulations2D/gen_rock.m
+++ simulations2D/gen_rock.m
@@ -6,12 +6,12 @@ facies_name = sprintf('facies/facies%d.mat', realization);
 poro_name = sprintf('porosity/porosity%d.mat', realization);
 perm_name = sprintf('permeability/logperm%d.mat', realization);
 
-facies_dat = load(fullfile(pwd(), facies_name)).facies'+0.5;
+facies_dat = load(fullfile(pwd(), facies_name)).facies'+0.66;
 poro_dat   = load(fullfile(pwd(), poro_name)).poro;
-perm_dat   = 10.^load(fullfile(pwd(), perm_name)).logperm;
+perm_dat   = 10.^load(fullfile(pwd(), perm_name)).logperm *milli*darcy;
 
 poro_sample = facies_dat .* poro_dat;
-perm_sample = facies_dat .* convertFrom(perm_dat, milli*darcy) + 10*milli*darcy;
+perm_sample = (10.^(facies_dat .* log10(convertTo(perm_dat,milli*darcy))))*milli*darcy;
 
 perm(:,1) = perm_sample;
 perm(:,2) = perm_sample;
