4a8a7fc36dcef3e6b9598f620551beeecae0c8f7
Misael M. Morales <misaelmiguelmorales@gmail.com>
2023-08-12 01:53:56 -0500

    update

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ import os
 from time import time
 import numpy as np
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 from scipy.io import loadmat, savemat
 from sklearn.preprocessing import MinMaxScaler
@@ -14,10 +16,12 @@ import tensorflow as tf
 import keras.backend as K
 from keras import Model, regularizers
 from keras.layers import *
-from keras.optimizers import Adam, Nadam
-from tensorflow_addons.layers import InstanceNormalization, GELU
+from tensorflow_addons.layers import *
+from keras.optimizers import *
+from keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.image import ssim as SSIM
+from tensorflow.image import ssim_multiscale as MSSIM
 from tensorflow.keras.callbacks import LearningRateScheduler
-from tensorflow.image import ssim
 
 def check_tensorflow_gpu():
     sys_info = tf.sysconfig.get_build_info()
@@ -40,11 +44,13 @@ class SpatiotemporalCO2:
         self.dim        = 64
         self.test_size  = 0.25
         self.optimizer  = Nadam(learning_rate=1e-3)
-        self.loss_alpha = 0.5
+        self.criterion  = self.custom_loss
+        self.l2_alpha   = 0.8
         self.num_epochs = 100
         self.batch_size = 30
         self.monitor_cb = 10
         self.lr_cb      = 20
+        self.verbose    = 0
 
     def load_data(self):
         X_data = np.zeros((self.n_realizations, self.x_channels, self.dim, self.dim))
@@ -55,7 +61,7 @@ class SpatiotemporalCO2:
         self.X_data, self.y_data = np.moveaxis(X_data, 1, -1), np.moveaxis(y_data, 2, -1)
         print('X: {} | y: {}'.format(self.X_data.shape, self.y_data.shape))
 
-    def process_data(self):
+    def process_data(self, subsample=None):
         num, height, width, channels = self.X_data.shape
         X_reshaped  = self.X_data.reshape(num*height*width, channels)
         X_scaled    = MinMaxScaler().fit_transform(X_reshaped)
@@ -65,25 +71,27 @@ class SpatiotemporalCO2:
         y_scaled    = MinMaxScaler().fit_transform(y_reshaped)
         self.y_norm = y_scaled.reshape(num, tsteps, height, width, channels)
         print('normalized - X: {} | y: {}'.format(self.X_norm.shape, self.y_norm.shape)) 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_norm, self.y_norm, test_size=self.test_size)
+        if subsample != None:
+            idx = np.random.choice(range(self.n_realizations), subsample, replace=False)
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_norm[idx], self.y_norm[idx], test_size=self.test_size)
+        else:
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_norm, self.y_norm, test_size=self.test_size)
         print('Train - X: {} | y: {}'.format(self.X_train.shape, self.y_train.shape))
         print('Test  - X: {} | y: {}'.format(self.X_test.shape, self.y_test.shape))
 
-    def plot_loss(self):
-        ep = len(self.fit.history['loss'])
+    def plot_loss(self, fit):
+        ep = len(fit.history['loss'])
         it = np.arange(ep)
-        plt.plot(it, self.fit.history['loss'], '-', label='loss')
-        plt.plot(it, self.fit.history['val_loss'], '-', label='validation loss')
+        plt.plot(it, fit.history['loss'], '-', label='loss')
+        plt.plot(it, fit.history['val_loss'], '-', label='validation loss')
         plt.title('Training: Loss vs. Epochs'); plt.legend()
         plt.xlabel('Epochs'); plt.ylabel('Loss')
         plt.xticks(it[::ep//10]); plt.show()
 
     def custom_loss(self, true, pred):
-        mse = tf.keras.losses.MeanSquaredError()
-        mse_loss = mse(true, pred)
-        ssim_losses = [1.0 - ssim(true[:, t], pred[:, t], max_val=1.0) for t in range(true.shape[1])]
-        ssim_loss = tf.reduce_mean(ssim_losses)
-        combined_loss = self.loss_alpha * ssim_loss + (1-self.loss_alpha) * mse_loss
+        mse_loss = MeanSquaredError()(true, pred)
+        ssim_loss = tf.reduce_mean(1.0 - SSIM(true, pred, max_val=1.0))
+        combined_loss = self.l2_alpha * mse_loss + (1-self.l2_alpha) * ssim_loss
         return combined_loss
 
     class LossCallback(tf.keras.callbacks.Callback):
@@ -97,63 +105,55 @@ class SpatiotemporalCO2:
             return new_lr
         return lr
 
-    def conv_block(self, inp, filt, kern=(3,3), pool=(2,2), pad='same'):
+    def encoder_layer(self, inp, filt, kern=(3,3), pool=(2,2), pad='same', leaky_slope=0.1):
         x = SeparableConv2D(filters=filt, kernel_size=kern, padding=pad)(inp)
-        x = SeparableConv2D(filters=filt, kernel_size=kern, padding=pad)(x)
+        #x = SeparableConv2D(filters=filt, kernel_size=kern, padding=pad)(x)
         x = InstanceNormalization()(x)
-        x = GELU()(x)
-        x = AveragePooling2D(pool)(x)
+        x = LeakyReLU(leaky_slope)(x)
+        x = MaxPooling2D(pool)(x)
         return x
 
-    def decon_block(self, inp, filt, kern=(3,3), pad='same'):
-        x = TimeDistributed(Conv2DTranspose(filters=filt, kernel_size=kern, padding=pad, strides=1))(inp)
+    def decoder_layer(self, inp, filt, kern=(3,3), pad='same', drop=0.1, leaky_slope=0.1):
+        x = ConvLSTM2D(filters=filt, kernel_size=kern, padding=pad, return_sequences=True, dropout=drop)(inp)
+        #x = TimeDistributed(SeparableConv2D(filters=filt, kernel_size=kern, padding=pad))(x)
         x = TimeDistributed(Conv2DTranspose(filters=filt, kernel_size=kern, padding=pad, strides=2))(x)
         x = InstanceNormalization()(x)
-        x = GELU()(x)
+        x = LeakyReLU(leaky_slope)(x)
         return x
-    
+       
     def make_model(self):
-        K.clear_session()
         inp = Input(shape=(self.dim, self.dim, self.x_channels))
         # Encoder
-        _ = self.conv_block(inp, 16)
-        _ = self.conv_block(_,   32)
-        z = self.conv_block(_,   64)
-        # Recurrent
-        _ = Flatten()(z)
-        _ = RepeatVector(n=8*8*64)(_)
-        _ = LSTM(units=60, return_sequences=True, dropout=0.2)(_)
-        _ = tf.transpose(_, [0, 2, 1])
-        zt = Reshape((60,8,8,64))(_)
+        _ = self.encoder_layer(inp, 16)
+        _ = self.encoder_layer(_,   32)
+        _ = self.encoder_layer(_,   64)
+        # Recurrence
+        _ = tf.expand_dims(_, 1)
+        _ = tf.tile(_, (1, self.timesteps, 1, 1, 1))
         # Decoder
-        _ = self.decon_block(zt, 64)
-        _ = self.decon_block(_, 32)
-        _ = self.decon_block(_, 16)
-        out = TimeDistributed(Conv2D(filters=2, kernel_size=(3,3), padding='same'))(_)
+        _ = self.decoder_layer(_, 64)
+        _ = self.decoder_layer(_, 32)
+        _ = self.decoder_layer(_, 16)
+        out = TimeDistributed(Conv2D(self.y_channels, (3,3), padding='same'))(_)
         # Output
-        self.encoder = Model(inp, z)
-        self.dynamic = Model(z, zt)
-        self.decoder_static  = Model(z, out)
-        self.decoder_dynamic = Model(zt, out)
-        self.model   = Model(inp, out)
-        #
+        self.model = Model(inp, out)
         n_params = self.model.count_params()
         print('# Parameters: {:,}'.format(n_params))
         return self.model
 
-    def train(self):
-        self.model.compile(optimizer=self.optimizer, loss=self.custom_loss, metrics=['mse'])
-        loss_callback = self.LossCallback()
-        lr_schedule = LearningRateScheduler(self.lr_scheduler)
+    def train(self, model):
+        model.compile(optimizer=self.optimizer, loss=self.criterion, metrics=['mse'])
+        #loss_callback = self.LossCallback()
+        #lr_schedule = LearningRateScheduler(self.lr_scheduler)
         start = time()
-        self.fit = self.model.fit(self.X_train, self.y_train,
-                                    shuffle          = True,
-                                    epochs           = self.num_epochs,
-                                    validation_split = 0.20,
-                                    batch_size       = self.batch_size,
-                                    callbacks        = [loss_callback, lr_schedule],
-                                    verbose          = 0)
+        fit = model.fit(self.X_train, self.y_train,
+                        epochs           = self.num_epochs,
+                        batch_size       = self.batch_size,
+                        validation_split = 0.20,
+                        shuffle          = True,
+                        #callbacks        = [loss_callback, lr_schedule],
+                        verbose          = self.verbose)
         train_time = time()-start
         print('Training Time: {:.2f} minutes'.format(train_time/60))
-        self.plot_loss()
-        return self.fit
+        self.plot_loss(fit)
+        return model, fit
